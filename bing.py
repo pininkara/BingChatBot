ead1a75f5aee855842ad522aa762ce2f5471b7b6
pininkara <pininkara@outlook.com>
2023-04-27 17:06:21 +0800

    1.Add group chat support 2.Add conversation style switch

--- bing.py
+++ bing.py
@@ -1,104 +1,148 @@
-import telebot
 import asyncio
-import re
 import json
 import os
+import re
 
-from telebot.async_telebot import AsyncTeleBot
-
+import telebot
 from EdgeGPT import Chatbot, ConversationStyle
 from telebot.util import quick_markup
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 ALLOWED_USER_IDS = os.getenv('ALLOWED_USER_IDS').split(',')
+BOT_ID = os.getenv('BOT_ID', '')
 COOKIE_PATH = os.getenv('COOKIE_PATH', './cookie.json')
+GROUP_MODE = os.getenv('GROUP_MODE', False)
+PUBLIC_MODE = os.getenv('PUBLIC_MODE', False)
+
+print("The startup is successful, the configuration is as follows : ")
+print("BOT_TOKEN: " + BOT_TOKEN)
+print("ALLOWED_USER_IDS: ")
+print(ALLOWED_USER_IDS)
+print("COOKIE_PATH: " + COOKIE_PATH)
+print("GROUP_MODE: " + str(GROUP_MODE))
+print("PUBLIC_MODE: " + str(PUBLIC_MODE))
 
 bot = telebot.TeleBot(BOT_TOKEN)
 EDGES = {}
-for user in ALLOWED_USER_IDS:
-    EDGES[int(user)] = Chatbot(cookie_path=COOKIE_PATH)
 
 not_allow_info = '⚠️You are not authorized to use this bot⚠️'
 
 markup = quick_markup({
     'Github': {'url': 'https://github.com/pininkara/BingChatBot'},
 }, row_width=1)
+conversation_style = ConversationStyle.balanced
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    if is_allowed(message):
+    if is_allowed(message) or PUBLIC_MODE or message.chat.type == "group":
         bot.reply_to(
-            message, "Bing Chat Bot By Kakanya~\n", reply_markup=markup)
+            message, "Bing Chat Bot By Kakanya~\n/help - Show help message\n/reset - Reset conversation\n/switch - "
+                     "Switch conversation style (creative,balanced,precise)\n", reply_markup=markup)
     else:
         bot.reply_to(message, not_allow_info)
 
 
 @bot.message_handler(commands=['reset'])
 def send_reset(message):
-    if is_allowed(message):
+    if is_allowed(message) or PUBLIC_MODE or message.chat.type == "group":
+        if message.from_user.id not in EDGES:
+            EDGES[message.from_user.id] = Chatbot(cookie_path=COOKIE_PATH)
         asyncio.run(EDGES[message.from_user.id].reset())
         bot.reply_to(message, "Reset successful🎉")
     else:
         bot.reply_to(message, not_allow_info)
 
 
-@bot.message_handler(func=lambda msg: True)
-def response_all(message):
-    print("Receive: " + message.text)
+@bot.message_handler(commands=['switch'])
+def switch_style(message):
     if is_allowed(message):
-        responseList = asyncio.run(bingChat(message.text, message))
-
-        if len(responseList[0]) > 4095:
-            for x in range(0, len(responseList[0]), 4095):
+        list = message.text.split(" ")
+        if len(list) > 1:
+            if list[1] == "creative":
+                conversation_style = ConversationStyle.creative
                 bot.reply_to(
-                    message, responseList[0][x:x + 4095], parse_mode='Markdown', reply_markup=responseList[1])
+                    message,
+                    "Switch successful , current style is creative")
+            elif list[1] == "balanced":
+                conversation_style = ConversationStyle.balanced
+                bot.reply_to(
+                    message,
+                    "Switch successful , current style is balanced")
+            elif list[1] == "precise":
+                conversation_style = ConversationStyle.precise
+                bot.reply_to(
+                    message,
+                    "Switch successful , current style is precise")
+            else:
+                bot.reply_to(
+                    message, "Parameter error , please choose one of (creative,balanced,precise)\n(e.g./switch balanced")
         else:
             bot.reply_to(
-                message, responseList[0], parse_mode='Markdown', reply_markup=responseList[1])
+                message, "Parameter error , please choose one of (creative,balanced,precise)\n(e.g./switch balanced")
     else:
         bot.reply_to(message, not_allow_info)
 
 
-@bot.callback_query_handler(func=lambda msg: True)
-def callback_all(callbackQuery):
-    print("callbackQuery: " + callbackQuery.data)
-    responseList = asyncio.run(bingChat(callbackQuery.data, callbackQuery))
+@bot.message_handler(func=lambda msg: True)
+def response_all(message):
+    print("Message: " + message.text)
+    if message.chat.type == "private" or GROUP_MODE or message.text.startswith(BOT_ID):
+        if is_allowed(message) or PUBLIC_MODE or message.chat.type == "group":
+            response_list = asyncio.run(bing_chat(message.text, message))
+            if len(response_list[0]) > 4095:
+                for x in range(0, len(response_list[0]), 4095):
+                    bot.reply_to(
+                        message, response_list[0][x:x + 4095], parse_mode='Markdown', reply_markup=response_list[1])
+            else:
+                bot.reply_to(
+                    message, response_list[0], parse_mode='Markdown', reply_markup=response_list[1])
+        else:
+            bot.reply_to(message, not_allow_info)
 
-    if len(responseList[0]) > 4095:
-        for x in range(0, len(responseList[0]), 4095):
+
+@bot.callback_query_handler(func=lambda msg: True)
+def callback_all(callback_query):
+    print("callbackQuery: " + callback_query.data)
+    response_list = asyncio.run(bing_chat(callback_query.data, callback_query))
+
+    if len(response_list[0]) > 4095:
+        for x in range(0, len(response_list[0]), 4095):
             bot.reply_to(
-                callbackQuery.message, responseList[0][x:x + 4095], parse_mode='Markdown', reply_markup=responseList[1])
+                callback_query.message, response_list[0][x:x + 4095], parse_mode='Markdown',
+                reply_markup=response_list[1])
 
     else:
         bot.reply_to(
-            callbackQuery.message, responseList[0], parse_mode='Markdown', reply_markup=responseList[1])
+            callback_query.message, response_list[0], parse_mode='Markdown', reply_markup=response_list[1])
 
 
-async def bingChat(messageText, message):
-    response_dict = await EDGES[message.from_user.id].ask(prompt=messageText,
-                                                          conversation_style=ConversationStyle.creative)
+async def bing_chat(message_text, message):
+    if message.from_user.id not in EDGES:
+        EDGES[message.from_user.id] = Chatbot(cookie_path=COOKIE_PATH)
+    response_dict = await EDGES[message.from_user.id].ask(prompt=message_text,
+                                                          conversation_style=conversation_style)
 
     json_str = json.dumps(response_dict)
     print("JSON: \n" + json_str)
 
     if 'text' in response_dict['item']['messages'][1]:
-        response = re.sub(r'\[\^\d\^\]', '',
+        response = re.sub(r'\[\^\d\^]', '',
                           response_dict['item']['messages'][1]['text'])
     else:
         response = "Something wrong. Please reset chat"
 
     if 'suggestedResponses' in response_dict['item']['messages'][1]:
-        suggestedResponses0 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['suggestedResponses'][0]['text'])
-        suggestedResponses1 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['suggestedResponses'][1]['text'])
-        suggestedResponses2 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['suggestedResponses'][2]['text'])
+        suggested_responses0 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['suggestedResponses'][0]['text'])
+        suggested_responses1 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['suggestedResponses'][1]['text'])
+        suggested_responses2 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['suggestedResponses'][2]['text'])
         markup = quick_markup({
-            suggestedResponses0: {'callback_data': suggestedResponses0[0:21]},
-            suggestedResponses1: {'callback_data': suggestedResponses1[0:21]},
-            suggestedResponses2: {'callback_data': suggestedResponses2[0:21]}
+            suggested_responses0: {'callback_data': suggested_responses0[0:21]},
+            suggested_responses1: {'callback_data': suggested_responses1[0:21]},
+            suggested_responses2: {'callback_data': suggested_responses2[0:21]}
         }, row_width=1)
     else:
         markup = quick_markup({
@@ -107,45 +151,45 @@ async def bingChat(messageText, message):
 
     if 'maxNumUserMessagesInConversation' in response_dict['item']['throttling'] and 'numUserMessagesInConversation' in \
             response_dict['item']['throttling']:
-        maxNumUserMessagesInConversation = response_dict['item'][
+        max_num_user_messages_in_conversation = response_dict['item'][
             'throttling']['maxNumUserMessagesInConversation']
-        numUserMessagesInConversation = response_dict['item']['throttling']['numUserMessagesInConversation']
+        num_user_messages_in_conversation = response_dict['item']['throttling']['numUserMessagesInConversation']
         response = response + "\n----------\n"
         response = response + "Messages In Conversation : %d / %d" % (
-            numUserMessagesInConversation, maxNumUserMessagesInConversation)
+            num_user_messages_in_conversation, max_num_user_messages_in_conversation)
 
-    if numUserMessagesInConversation >= maxNumUserMessagesInConversation:
-        await EDGES[messageText.from_user.id].reset()
+    if num_user_messages_in_conversation >= max_num_user_messages_in_conversation:
+        await EDGES[message_text.from_user.id].reset()
         response = response + "\nAutomatic reset succeeded🎉"
 
     if len(response_dict['item']['messages'][1]['sourceAttributions']) >= 3:
-        providerDisplayName0 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['sourceAttributions'][0]['providerDisplayName'])
-        seeMoreUrl0 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['sourceAttributions'][0]['seeMoreUrl'])
-        providerDisplayName1 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['sourceAttributions'][1]['providerDisplayName'])
-        seeMoreUrl1 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['sourceAttributions'][1]['seeMoreUrl'])
-        providerDisplayName2 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['sourceAttributions'][2]['providerDisplayName'])
-        seeMoreUrl2 = re.sub(
-            r'\[\^\d\^\]', '', response_dict['item']['messages'][1]['sourceAttributions'][2]['seeMoreUrl'])
+        provider_display_name0 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['sourceAttributions'][0]['providerDisplayName'])
+        see_more_url0 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['sourceAttributions'][0]['seeMoreUrl'])
+        provider_display_name1 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['sourceAttributions'][1]['providerDisplayName'])
+        see_more_url1 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['sourceAttributions'][1]['seeMoreUrl'])
+        provider_display_name2 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['sourceAttributions'][2]['providerDisplayName'])
+        see_more_url2 = re.sub(
+            r'\[\^\d\^]', '', response_dict['item']['messages'][1]['sourceAttributions'][2]['seeMoreUrl'])
         response = response + "\n----------\nReference:\n"
         response = response + \
-            "1.[%s](%s)\n" % (providerDisplayName0, seeMoreUrl0)
+                   "1.[%s](%s)\n" % (provider_display_name0, see_more_url0)
         response = response + \
-            "2.[%s](%s)\n" % (providerDisplayName1, seeMoreUrl1)
+                   "2.[%s](%s)\n" % (provider_display_name1, see_more_url1)
         response = response + \
-            "3.[%s](%s)\n" % (providerDisplayName2, seeMoreUrl2)
+                   "3.[%s](%s)\n" % (provider_display_name2, see_more_url2)
 
     markup = quick_markup({
-        suggestedResponses0: {'callback_data': suggestedResponses0[0:21]},
-        suggestedResponses1: {'callback_data': suggestedResponses1[0:21]},
-        suggestedResponses2: {'callback_data': suggestedResponses2[0:21]}
+        suggested_responses0: {'callback_data': suggested_responses0[0:21]},
+        suggested_responses1: {'callback_data': suggested_responses1[0:21]},
+        suggested_responses2: {'callback_data': suggested_responses2[0:21]}
     }, row_width=1)
-    responseList = [response, markup]
-    return responseList
+    response_list = [response, markup]
+    return response_list
 
 
 def is_allowed(message) -> bool:
